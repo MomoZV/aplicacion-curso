33d6d865e71778758d902de8bae5aefc5961ccdd
Manuel Zuñiga <moleconpollo1@gmail.com>
2024-03-14 12:30:46 -0600

    Cambios de la primera sesión

--- ProyectoAplicacionPrueba/app/src/main/java/com/example/proyectoaplicacionprueba/ui/home/HomeFragment.kt
+++ ProyectoAplicacionPrueba/app/src/main/java/com/example/proyectoaplicacionprueba/ui/home/HomeFragment.kt
@@ -8,6 +8,8 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.proyectoaplicacionprueba.databinding.FragmentHomeBinding
+import kotlin.math.log
+import kotlin.random.Random
 
 class HomeFragment : Fragment() {
 
@@ -30,8 +32,38 @@ class HomeFragment : Fragment() {
 
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+            //textView.text = "Has presionado el boton " + 0 + " veces"
+            textView.text = "Lista de animales: \n"
         }
+
+        var Boton = binding.button
+        var contador = 0
+        val catalagoAnimales = arrayOf("Perro", "Gato", "Leon", "Jirafa", "Pajaro", "Tortuga")
+        //val catalagoAnimales = arrayOf("Perro")
+        var animalSeleccionado = 0
+
+        ////////////////ESTO ES LO QUE HACE FUNCIONAR AL BOTON////////////////
+        Boton.setOnClickListener(){
+            /*contador = contador + 1
+            textView.text = "Has presionado el boton " + contador + " veces"*/
+
+            /*
+            animalSeleccionado = Random.nextInt(0, 6)
+            println(animalSeleccionado)
+            if(animalSeleccionado > catalagoAnimales.size)
+                textView.text = "Este animal no esta en el catalogo"
+            else
+                textView.text = "El animal seleccionado fue " + catalagoAnimales[animalSeleccionado]
+            */
+
+            animalSeleccionado = Random.nextInt(0, 6)
+            textView.text = textView.text.toString() + catalagoAnimales[animalSeleccionado] + "\n"
+
+        }
+        //////////////////////////////////////////////////////////////////////
+
+
+
         return root
     }
 
